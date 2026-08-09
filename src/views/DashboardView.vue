d6aed6b1c2bc7be767b907f305b0ac1a9924368d
minxvyv <minxvyv@163.com>
2026-08-08 23:41:24 -0400

    fix: correct ground control steering direction

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1093,7 +1093,7 @@ watch(() => [controlStatus.value.state, controlStatus.value.transitioning], ([st
 
 const handleLeftStick = (vec) => controlState.value.throttle = vec.y;
 const resetLeftStick = () => controlState.value.throttle = 0;
-const handleRightStick = (vec) => controlState.value.steering = vec.x;
+const handleRightStick = (vec) => controlState.value.steering = -vec.x;
 const resetRightStick = () => controlState.value.steering = 0;
 
 onUnmounted(() => {
